f9461d45502f496fae974f853b76ec46d7b9bb13
seoyoungsoo <iulove37@naver.com>
2021-03-26 22:04:50 +0900

    Lv1

--- Programmers/Lv1/2016년.js
+++ Programmers/Lv1/2016년.js
@@ -7,3 +7,17 @@ const solution = (a, b) => {
 const a = 5;
 const b = 24;
 console.log(solution(a, b));
+
+// Date 생성자를 사용하지 않고 풀이
+// const solution2 = (a, b) => {
+//   const day = ['THU', 'FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED'];
+//   const month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+//   let cnt = 0;
+//   for (let i = 0; i < a-1; i++) {
+//     cnt += month[i];
+//   }
+
+//   cnt += b;
+//   return day[cnt % 7];
+// }
