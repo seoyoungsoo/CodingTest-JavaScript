ba32a54a048d1d011aa1d4c15f0b8f5ee42e403e
seoyoungsoo <iulove37@naver.com>
2021-04-25 20:43:51 +0900

    lc 2798

---
+++ BOJ/BruteForce/2798_블랙잭.js
@@ -0,0 +1,30 @@
+// 2798번 블랙잭
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const input = [];
+
+rl.on('line', (line) => {
+  input.push(line.split(' ').map(n => parseInt(n)));
+}).on(('close'), () => {
+  const N = input[0][0];
+  const M = input[0][1];
+  const cards = input[1];
+
+  let state = 0;
+  for (let i = 0; i < cards.length - 2; i++) {
+    for (let j = i + 1; j < cards.length - 1; j++) {
+      for (let k = j + 1; k < cards.length; k++) {
+        let tmp = cards[i] + cards[j] + cards[k];
+        if (tmp > state && tmp <= M) state = tmp;
+      }
+    }
+  }
+  console.log(state);
+
+  process.exit();
+})
